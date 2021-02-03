ba3c855303417e2d2111a479296051c4e7dfc3e6
Panu Hokkanen <cccpanuchka@gmail.com>
2021-02-03 15:31:20 +0200

    Add Recast

--- src/Game.ts
+++ src/Game.ts
@@ -1,9 +1,12 @@
-import { NullEngine } from '@babylonjs/core'
+import * as Recast from 'recast-detour'
+import { NullEngine, RecastJSPlugin } from '@babylonjs/core'
 
 export default class Game {
   engine: NullEngine
+  navigation: RecastJSPlugin
 
   constructor() {
     this.engine = new NullEngine()
+    this.navigation = new RecastJSPlugin(Recast)
   } 
 }
